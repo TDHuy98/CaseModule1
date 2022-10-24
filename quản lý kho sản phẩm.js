0c50d769270e7076f05f68e530b8472bc55a269c
TDHuy98 <112893732+TDHuy98@users.noreply.github.com>
2022-10-24 10:11:15 +0700

    Before store array to localstorage

---
+++ quản lý kho sản phẩm.js
@@ -0,0 +1,229 @@
+class item {
+    brand;
+    name;
+    amount;
+
+    constructor(brand, name, amount) {
+        this.brand = brand;
+        this.name = name;
+        this.amount = amount;
+    }
+
+    getbrand() {
+        return this.brand;
+    }
+
+    getname() {
+        return this.name;
+    }
+
+    getamount() {
+        return this.amount;
+    }
+
+    setbrand(brand) {
+        return this.brand = brand;
+    }
+
+    setname(name) {
+        return this.name = name;
+    }
+
+    setamount(amount) {
+        return this.amount = amount
+    }
+}
+
+var product = [new item('Sony','XperiaX',100),new item('Asus','ROG_Phone_6',9),new item('SamSung','ZFold4',6)];
+var stored
+localStorage.setItem(stored,JSON.stringify(product));
+var storedproduct=JSON.parse(localStorage.getItem(stored));
+console.log(storedproduct)
+function hienthisp() {
+    let stt = 1;
+    let chuoicaccaulenh = '';
+    for (let i = 0; i < product.length; i++, stt++) {
+        if (product[i] !== 'empty') {
+            let caulenh = '<tr class="tablelist">' + '<td>' + stt + '</td>'
+                + '<td class="tablelist">' + product[i].getbrand() + '</td>'
+                + '<td class="tablelist">' + product[i].getname() + '</td>'
+                + '<td class="tablelist">' + product[i].getamount() + '</td>'
+                + '<td class="tablelist">' + '<button onclick=' + 'suatensp("' + product[i].getname() + '")>' + 'Sửa' + '</button>'
+                + '</td>'
+                + '<td class="tablelist">' + '<button onclick=' + 'xoasp("' + product[i].getname() + '")>' + 'Xóa' + '</button>'
+                + '</td>'
+                + '</tr>';
+            chuoicaccaulenh += caulenh
+        }
+    }
+    document.getElementById('p1').innerHTML =
+
+        '<table class="tablelist">'
+        + '<tr class="tablelist">'
+        + '<th class="trlist">STT</th>'
+        + '<th class="tablelist">Hãng</th>'
+        + '<th class="tablelist">Model</th>'
+        + '<th class="tablelist">Số lượng</th>'
+        + '<th class="tablelist" colspan="2">Tùy chỉnh</th>'
+        + '</tr>' + chuoicaccaulenh
+        + '</table>';
+}
+
+function themsp() {
+    let brand = document.getElementById('spthem').value;
+    let model = document.getElementById('nameinp').value;
+    let amount = document.getElementById('amountinp').value;
+    if (brand != '' && model != '' & amount != '') {
+        let a = new item(brand, model, amount)
+        product.push(a)
+        localStorage.setItem(classstorage,product)
+
+        hienthisp()
+    } else alert('Hãy nhập thông tin vật phẩm muốn thêm')
+}
+
+function suatensp(suaten) {
+    for (let i = 0; i < product.length; i++) {
+        let nhap;
+        if (suaten === product[i].getname()) {
+            nhap = prompt('Sửa số lượng sản phẩm: ');
+            while (nhap == '') {
+                nhap = prompt('Sửa số lượng sản phẩm: ');
+            }
+            product[i].setamount(nhap);
+            hienthisp()
+        }
+    }
+}
+
+function xoasp(spxoa) {
+    for (let i = 0; i < product.length; i++) {
+        if (spxoa === product[i].name) {
+            product.splice(i, 1);
+            hienthisp()
+        }
+    }
+}
+
+function sortarrayaz() {
+    product.sort(function (a, b) {
+        if (a.getbrand() < b.getbrand()) {
+            return -1;
+        }
+        if (a.getbrand() > b.getbrand()) {
+            return 1;
+        }
+    })
+    hienthisp()
+}
+
+function sortarrayza() {
+
+    product.sort(function (a, b) {
+        if (a.getbrand() < b.getbrand()) {
+            return 1;
+        }
+        if (a.getbrand() > b.getbrand()) {
+            return -1;
+        }
+    })
+    hienthisp()
+}
+
+function sortmodelaz() {
+    product.sort(function (a, b) {
+        if (a.getname() < b.getname()) {
+            return -1;
+        }
+        if (a.getname() > b.getname()) {
+            return 1;
+        }
+    })
+    hienthisp()
+}
+
+function sortmodelza() {
+
+    product.sort(function (a, b) {
+        if (a.getname() < b.getname()) {
+            return 1;
+        }
+        if (a.getname() > b.getname()) {
+            return -1;
+        }
+    })
+    hienthisp()
+}
+
+function sortamountgl() {
+
+    product.sort(function (a, b) {
+        if (a.getamount() < b.getamount()) {
+            return -1;
+        }
+        if (a.getamount() > b.getamount()) {
+            return 1;
+        }
+    })
+    hienthisp()
+}
+
+function sortamountlg() {
+
+    product.sort(function (a, b) {
+        if (a.getamount() < b.getamount()) {
+            return 1;
+        }
+        if (a.getamount() > b.getamount()) {
+            return -1;
+        }
+    })
+    hienthisp()
+}
+
+function timkiem() {
+    let tim = document.getElementById('search').value;
+    for (let i = 0; i < product.length - 1; i++) {
+        if (tim == product[i].getbrand() || tim == product[i].getname()) {
+
+        }
+    }
+}
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+// function selectsort(){
+//     let type=document.getElementById('sortitem').value;
+//     if (type == bybrandaz){
+//         sortarrayaz()
+//     }else if (type == bybrandza){
+//         sortarrayza()
+//     }else if (type==bymodelaz){
+//         sortmodelaz()
+//     }else if (type==bymodelza){
+//         sortmodelza()
+//     }else if (type==byamountgl){
+//         sortamountgl()
+//     }else sortamountlg()
+// }
